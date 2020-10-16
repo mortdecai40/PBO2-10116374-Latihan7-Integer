82bfbc76c22cf57160e79c3629ab6fbce0c8c316
mortdecai40 <rizkinabil.999@gmail.com>
2020-10-16 14:05:52 +0700

    Program Integer

---
+++ src/pbo2/pkg10116374/latihan7/integer/PBO210116374Latihan7Integer.java
@@ -0,0 +1,30 @@
+package pbo2.pkg10116374.latihan7.integer;
+
+/**
+ * @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk memasukan bilangan integer
+ */
+public class PBO210116374Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      byte b;
+      short s;
+      int i;
+      long l;
+      b = 120;
+      s = 32767;
+      i = 1_000_000_000;
+      l = 90000000000L;
+      System.out.println("byte \t:" +b);
+      System.out.println("short \t:" + s);
+      System.out.println("int \t:" +i);
+      System.out.println("long \t:" +l);
+    }
+    
+}
